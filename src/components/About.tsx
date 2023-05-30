6bdc88dfda4c3df539ac809c0ab1fb7d0900e802
gabeyamartino <gabeyamartino@gmail.com>
2023-05-30 13:52:27 -0400

    Apply section spacing to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@
 function About() {
 
   return (
-    <section className="">
-      <div>I'm an About section!</div>
+    <section className="max-h-sectionMax min-h-sectionMin bg-blue-400">
+      <div className="h-screen">I'm an About Me section!</div>
     </section>
   )
 }
