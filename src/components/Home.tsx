e181d6ebcbb692974278808e1ccc939ff3e2bc44
gabeyamartino <gabeyamartino@gmail.com>
2023-06-01 11:39:24 -0400

    Implement media query on Home sectionto accomodate mobile viewing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,9 @@
 function Home() {
   return (
-    <section className="max-h-sectionMax min-h-sectionMin bg-main-bg">
-      <div className="h-screen">I'm a Home section!
-        <div>
-
-        </div>
+    <section className="h-64 max-h-sectionMax min-h-sectionMin bg-main-bg relative sm:h-screen">
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-6xl uppercase tracking-wider text-center text-white">Hi, I'm Gabe Yamartino</h1>
+        <div className="text-white text-2xl text-center w-full leading-tight">Welcome to my site!</div>
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "600px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       maxHeight: {
         sectionMax: '60rem',
