2e422abc74c15566a583cd9d1477874b57177cde
gabeyamartino <gabeyamartino@gmail.com>
2023-06-01 10:06:13 -0400

    Add svg's for background, hamburger menu, and close menu buttons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@
 function About() {
 
   return (
-    <section className="max-h-sectionMax min-h-sectionMin bg-blue-400">
+    <section className="bg-blue-400">
       <div className="h-screen">I'm an About Me section!</div>
     </section>
   )
