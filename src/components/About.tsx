a136b9c2431086850ee6bef36a866c2e00f4b32e
gabeyamartino <gabeyamartino@gmail.com>
2023-06-14 13:44:19 -0400

    Begin styling About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,11 @@
 function About() {
 
   return (
-    <section className="bg-blue-400">
-      <div className="h-screen">I'm an About Me section!</div>
+    <section className="border border-black">
+      <div className="flex flex-col justify-center items-center py-16 sm:py-32 gap-6">
+        <h2 className="text-4xl text-bold uppercase tracking-wider text-center sm:text-5xl">About Me</h2>
+        <span className="w-10 h-2 p-0.5 bg-yellow-400 rounded"></span>
+      </div>
     </section>
   )
 }
