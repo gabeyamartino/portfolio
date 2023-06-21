b7759733f18b5c49590fa33fba7c961eb6c31617
gabeyamartino <gabeyamartino@gmail.com>
2023-06-21 13:00:37 -0400

    imported remaing project pictures. Formatting projects page to work on mobile and desktop. Began work on contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,8 +4,42 @@ type SectionProps = {
 
 const Contact: React.FC<SectionProps> = ({ id }) => {
   return (
-    <section id={id}>
-      <div>I'm a Contact section!</div>
+    <section
+      id={id}
+      className="flex h-full flex-col items-center justify-center space-y-8 bg-main-bg"
+    >
+      <div className="sm:py-30 mx-auto w-[92%] gap-6 pb-16 sm:w-5/6 sm:max-w-6xl">
+        <div className="flex flex-col items-center justify-center gap-6 py-16 ">
+          <h2 className="text-center text-4xl font-bold uppercase tracking-wider text-white sm:text-4xl lg:text-5xl">
+            Contact
+          </h2>
+          <span className="h-2 w-10 rounded bg-yellow-400"></span>
+          <span className="text-white sm:text-base sm:leading-relaxed xl:text-lg">
+            Please feel free to contact me by filling out the form below. I will
+            get back to you as soon as I can!
+          </span>
+        </div>
+        <div className="mx-auto w-full max-w-3xl  rounded-lg border bg-white p-16 shadow-lg">
+          <form action="#" method="post"></form>
+          <input type="hidden" name="form-name" value="form-1" />
+          <div className="mb-16 flex flex-col">
+            <label
+              className="mb-1 text-sm font-semibold sm:text-base"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className="w-full rounded-lg border border-gray-300 bg-gray-200 p-4 text-sm font-semibold leading-normal text-gray-900"
+              required
+              placeholder="Enter Your Name"
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import singSoftly from "../assets/singSoftlyMockup.png";
-import macBook from "../assets/macbookScreen.jpeg";
+import neighborly from "../assets/neighborly.png";
+import greyjoy from "../assets/greyjoy.png";
 
 type SectionProps = {
   id: string;
@@ -12,14 +13,14 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
       className="flex flex-col items-center justify-center bg-white"
     >
       <div className="sm:py-30 mx-auto w-[92%] gap-6 pb-16 sm:w-5/6 sm:max-w-6xl">
-        <div className="flex flex-col items-center justify-center gap-6 py-16 ">
+        <div className="flex flex-col items-center justify-center gap-16 py-16 ">
           <h2 className="text-center text-4xl font-bold uppercase tracking-wider sm:text-4xl lg:text-5xl">
             Projects
           </h2>
           <span className="h-2 w-10 rounded bg-yellow-400"></span>
         </div>
-        <div>
-          <div className="flex flex-col items-center justify-center gap-8 lg:flex-row">
+        <div className="">
+          <div className=" mb-24 flex flex-col items-center justify-center lg:flex-row">
             <div className="">
               <img
                 className="h-auto w-xxl"
@@ -34,7 +35,7 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
                 to easily find and listen to archived recordings of the band's
                 concerts
               </p>
-              <div className="flex gap-6">
+              <div className=" flex gap-6">
                 <a href="http://54.160.94.40:3000/" target="_blank">
                   <button className="my-8 transform rounded-lg bg-yellow-400 px-9 py-3 text-sm font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 hover:translate-y-[-5px] sm:text-base">
                     Live Link
@@ -51,12 +52,12 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
               </div>
             </div>
           </div>
-          <div className="my-8 flex flex-col items-center justify-center gap-8 lg:flex-row">
+          <div className="mb-24 flex flex-col items-center justify-center lg:flex-row">
             <div className="">
               <img
-                className="h-auto w-xxl"
-                src={macBook}
-                alt="Screenshot of Sing Softly Application"
+                className="h-auto w-xxl lg:mr-14"
+                src={neighborly}
+                alt="Screenshot of Neighborly Application"
               />
             </div>
             <div className="max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
@@ -77,9 +78,33 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
               </div>
             </div>
           </div>
+          <div className="mb-24 flex flex-col items-center justify-center lg:flex-row">
+            <div className="">
+              <img
+                className="h-auto w-xxl lg:mr-14"
+                src={greyjoy}
+                alt="Screenshot of Neighborly Application"
+              />
+            </div>
+            <div className="max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
+              <h2 className="mb-8 text-2xl font-bold">Atelier</h2>
+              <p>
+                A full stack community focused social networking platform driven
+                through user-administered groups
+              </p>
+              <div className=" flex gap-6">
+                <a
+                  href="https://github.com/gabeyamartino/SingSoft.ly"
+                  target="_blank"
+                >
+                  <button className="my-8 transform rounded-lg bg-yellow-400 px-9 py-3 text-sm font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 hover:translate-y-[-5px] sm:text-base">
+                    GitHub Repo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>Project 2</div>
-        <div>Project 3</div>
       </div>
     </section>
   );
