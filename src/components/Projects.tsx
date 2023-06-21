4eac1107e7dcabff9f9cdcf41f3d8e90dfbe8b7c
gabeyamartino <gabeyamartino@gmail.com>
2023-06-21 13:29:30 -0400

    Finalize styling on projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
       className="flex flex-col items-center justify-center bg-white"
     >
       <div className="sm:py-30 mx-auto w-[92%] gap-6 pb-16 sm:w-5/6 sm:max-w-6xl">
-        <div className="flex flex-col items-center justify-center gap-16 py-16 ">
+        <div className="flex flex-col items-center justify-center gap-6 py-16 ">
           <h2 className="text-center text-4xl font-bold uppercase tracking-wider sm:text-4xl lg:text-5xl">
             Projects
           </h2>
@@ -52,15 +52,15 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
               </div>
             </div>
           </div>
-          <div className="mb-24 flex flex-col items-center justify-center lg:flex-row">
+          <div className="mb-24 flex flex-col items-center justify-center lg:flex-row lg:gap-12">
             <div className="">
               <img
-                className="h-auto w-xxl lg:mr-14"
+                className="h-auto w-xxl"
                 src={neighborly}
                 alt="Screenshot of Neighborly Application"
               />
             </div>
-            <div className="max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
+            <div className="mt-11 max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
               <h2 className="mb-8 text-2xl font-bold">Neighborly</h2>
               <p>
                 A full stack community focused social networking platform driven
@@ -68,7 +68,7 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
               </p>
               <div className="flex gap-6">
                 <a
-                  href="https://github.com/gabeyamartino/SingSoft.ly"
+                  href="https://github.com/Neighborly-Saffron/Neighborly"
                   target="_blank"
                 >
                   <button className="my-8 transform rounded-lg bg-yellow-400 px-9 py-3 text-sm font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 hover:translate-y-[-5px] sm:text-base">
@@ -78,25 +78,22 @@ const Projects: React.FC<SectionProps> = ({ id }) => {
               </div>
             </div>
           </div>
-          <div className="mb-24 flex flex-col items-center justify-center lg:flex-row">
+          <div className=" flex flex-col items-center justify-center lg:flex-row lg:gap-12">
             <div className="">
               <img
-                className="h-auto w-xxl lg:mr-14"
+                className="h-auto w-xxl"
                 src={greyjoy}
-                alt="Screenshot of Neighborly Application"
+                alt="Screenshot of Atelier Application"
               />
             </div>
-            <div className="max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
+            <div className="mt-11 max-w-md sm:text-base sm:leading-relaxed xl:text-lg">
               <h2 className="mb-8 text-2xl font-bold">Atelier</h2>
               <p>
                 A full stack community focused social networking platform driven
                 through user-administered groups
               </p>
               <div className=" flex gap-6">
-                <a
-                  href="https://github.com/gabeyamartino/SingSoft.ly"
-                  target="_blank"
-                >
+                <a href="https://github.com/gabeyamartino/FEC" target="_blank">
                   <button className="my-8 transform rounded-lg bg-yellow-400 px-9 py-3 text-sm font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 hover:translate-y-[-5px] sm:text-base">
                     GitHub Repo
                   </button>
