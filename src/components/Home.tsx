10ab4867c98fb4018fe749371c9a39d34c3b1f90
gabeyamartino <gabeyamartino@gmail.com>
2023-06-28 11:31:43 -0400

    pdf filepath tweak testing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,8 +18,8 @@ const Home: React.FC<SectionProps> = ({ id, scrollToSection }) => {
           with a focus on user-centric design and seamless functionality.
         </div>
         <div className="flex gap-6">
-          <a href="../assets/Resume_Yamartino_Gabe.pdf" target="_blank">
-            <button className="sm: my-8 transform rounded-lg bg-yellow-400 px-9 py-3 font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 sm:hover:translate-y-[-5px]">
+          <a href="./assets/Resume_Yamartino_Gabe.pdf" target="_blank">
+            <button className="my-8 transform rounded-lg bg-yellow-400 px-9 py-3 font-semibold uppercase tracking-wide text-black shadow-lg transition duration-300 sm:hover:translate-y-[-5px]">
               Resume
             </button>
           </a>
